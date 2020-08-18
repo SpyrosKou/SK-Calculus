2237ec2683de301bd9b638b5e6164f73a9c5ee30
Spyros K <21125056+SpyrosKou@users.noreply.github.com>
2020-08-18 19:11:44 +0300

    Added license in tests

--- src/test/java/eu/spyros/koukas/sk_lamda_calculus/primary/IEquivalenceTest.java
+++ src/test/java/eu/spyros/koukas/sk_lamda_calculus/primary/IEquivalenceTest.java
@@ -1,10 +1,26 @@
 package eu.spyros.koukas.sk_lamda_calculus.primary;
-
+/**
+ * SK Calculus
+ *     A SK Combinator Calculus written in Java
+ *     https://github.com/SpyrosKou/SK-Calculus
+ *     Copyright (C) 2020  Spyros Koukas
+ *
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 import junit.framework.TestCase;
 
-import java.util.IdentityHashMap;
-
-import static eu.spyros.koukas.sk_lamda_calculus.primary.I.I;
 import static eu.spyros.koukas.sk_lamda_calculus.primary.K.K;
 import static eu.spyros.koukas.sk_lamda_calculus.primary.S.S;
 
