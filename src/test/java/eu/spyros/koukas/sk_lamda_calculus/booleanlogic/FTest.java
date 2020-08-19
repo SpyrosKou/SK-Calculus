0bb3bbe2bba724c1e3c4b422271d12807db72c5b
Spyros K <21125056+SpyrosKou@users.noreply.github.com>
2020-08-20 02:48:51 +0300

    Created tests for T and F

--- src/test/java/eu/spyros/koukas/sk_lamda_calculus/booleanlogic/FTest.java
+++ src/test/java/eu/spyros/koukas/sk_lamda_calculus/booleanlogic/FTest.java
@@ -4,16 +4,21 @@ import static eu.spyros.koukas.sk_lamda_calculus.primary.S.*;
 import static eu.spyros.koukas.sk_lamda_calculus.primary.K.*;
 import static eu.spyros.koukas.sk_lamda_calculus.booleanlogic.T.*;
 import static eu.spyros.koukas.sk_lamda_calculus.booleanlogic.F.*;
+
 import junit.framework.TestCase;
 
 /**
  * Created at 2020-08-19
  *
- * @author Spyros
+ * @author Spyros Koukas
  */
 public class FTest extends TestCase {
 
     public void testF() {
+        assertEquals(F, F().apply(T).apply(F));
+        assertEquals(T, F().apply(F).apply(T));
+        assertEquals(K, F().apply(F).apply(K));
+        assertEquals(S, F().apply(F).apply(S));
     }
 
     public void testFmethod() {
@@ -24,9 +29,40 @@ public class FTest extends TestCase {
     }
 
     public void testApply() {
+        assertEquals(F, F.apply(T).apply(F));
+        assertEquals(T, F.apply(F).apply(T));
+        assertEquals(K, F.apply(F).apply(K));
+        assertEquals(S, F.apply(F).apply(S));
+    }
+    public void testEquals() {
+        assertEquals(F, F());
+        assertEquals(F(), F());
 
+        assertEquals(F(), F(T).apply(F));
+        assertEquals(F(), F(F).apply(F));
+
+        assertFalse(F.equals(T()));
+        assertFalse(F().equals(T()));
+
+        assertFalse(F().equals(F(T).apply(T)));
+        assertFalse(F().equals(F(F).apply(T)));
+    }
+
+    public void testHashCode() {
+        assertEquals(F.hashCode(), F().hashCode());
+        assertEquals(F().hashCode(), F().hashCode());
+
+        assertEquals(F().hashCode(), F(T).apply(F).hashCode());
+        assertEquals(F().hashCode(), F(F).apply(F).hashCode());
     }
 
     public void testToString() {
+        final String expectedName = "F";
+        assertEquals(expectedName, F.toString());
+        assertEquals(expectedName, F().toString());
+    }
+
+    public void testSingleton() {
+        assertSame(F, F());
     }
 }
---
+++ src/test/java/eu/spyros/koukas/sk_lamda_calculus/booleanlogic/TTest.java
@@ -0,0 +1,88 @@
+package eu.spyros.koukas.sk_lamda_calculus.booleanlogic;
+
+import junit.framework.TestCase;
+
+import static eu.spyros.koukas.sk_lamda_calculus.booleanlogic.T.*;
+import static eu.spyros.koukas.sk_lamda_calculus.booleanlogic.F.*;
+
+/**
+ * Created at 2020-08-19
+ *
+ * @author Spyros Koukas
+ */
+public class TTest extends TestCase {
+
+    public void testHashCode() {
+        assertEquals(T.hashCode(), T().hashCode());
+        assertEquals(T().hashCode(), T().hashCode());
+
+        assertEquals(T().hashCode(), T(T).apply(T).hashCode());
+        assertEquals(T().hashCode(), T(T).apply(F).hashCode());
+    }
+
+    public void testEquals() {
+        assertEquals(T, T());
+        assertEquals(T(), T());
+
+        assertEquals(T(), T(T).apply(T));
+        assertEquals(T(), T(T).apply(F));
+
+        assertFalse(F.equals(T()));
+        assertFalse(F().equals(T()));
+
+        assertFalse(F().equals(T(T).apply(T)));
+        assertFalse(F().equals(T(T).apply(F)));
+    }
+
+    public void testTmethod() {
+        assertEquals(T, T(T).apply(T));
+        assertEquals(T, T(T).apply(F));
+        assertEquals(F, T(F).apply(T));
+        assertEquals(F, T(F).apply(F));
+
+    }
+
+    public void testApply() {
+        assertEquals(T, T.apply(T).apply(T));
+        assertEquals(T, T.apply(T).apply(F));
+        assertEquals(F, T.apply(F).apply(T));
+        assertEquals(F, T.apply(F).apply(F));
+    }
+
+    public void testT() {
+        assertEquals(T, T().apply(T).apply(T));
+        assertEquals(T, T().apply(T).apply(F));
+        assertEquals(F, T().apply(F).apply(T));
+        assertEquals(F, T().apply(F).apply(F));
+    }
+
+    public void testToString() {
+        assertEquals(T.toString(), T(T).apply(F).toString());
+        assertEquals(T.toString(), T(T).apply(T).toString());
+
+        assertEquals(F.toString(), T(F).apply(F).toString());
+        assertEquals(F.toString(), T(F).apply(T).toString());
+
+        assertEquals(T.toString(), T().apply(T).apply(F).toString());
+        assertEquals(T.toString(), T().apply(T).apply(T).toString());
+
+        assertEquals(F.toString(), T().apply(F).apply(F).toString());
+        assertEquals(F.toString(), T().apply(F).apply(T).toString());
+
+        assertEquals(T().toString(), T(T).apply(F).toString());
+        assertEquals(T().toString(), T(T).apply(T).toString());
+
+        assertEquals(F().toString(), T(F).apply(F).toString());
+        assertEquals(F().toString(), T(F).apply(T).toString());
+
+        assertEquals(T().toString(), T().apply(T).apply(F).toString());
+        assertEquals(T().toString(), T().apply(T).apply(T).toString());
+
+        assertEquals(F().toString(), T().apply(F).apply(F).toString());
+        assertEquals(F().toString(), T().apply(F).apply(T).toString());
+    }
+
+    public void testSingleton() {
+        assertSame(T, T());
+    }
+}
