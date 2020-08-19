0c840ad175ed68c70f16487b7477affc73f7e66d
Spyros K <21125056+SpyrosKou@users.noreply.github.com>
2020-08-19 16:05:55 +0300

    Created False test, partial

--- src/test/java/eu/spyros/koukas/sk_lamda_calculus/booleanlogic/FTest.java
+++ src/test/java/eu/spyros/koukas/sk_lamda_calculus/booleanlogic/FTest.java
@@ -1,7 +1,32 @@
+package eu.spyros.koukas.sk_lamda_calculus.booleanlogic;
+
+import static eu.spyros.koukas.sk_lamda_calculus.primary.S.*;
+import static eu.spyros.koukas.sk_lamda_calculus.primary.K.*;
+import static eu.spyros.koukas.sk_lamda_calculus.booleanlogic.T.*;
+import static eu.spyros.koukas.sk_lamda_calculus.booleanlogic.F.*;
 import junit.framework.TestCase;
+
 /**
  * Created at 2020-08-19
+ *
  * @author Spyros
- */public class FTest extends TestCase {
-  
+ */
+public class FTest extends TestCase {
+
+    public void testF() {
+    }
+
+    public void testFmethod() {
+        assertEquals(F, F(T).apply(F));
+        assertEquals(T, F(F).apply(T));
+        assertEquals(K, F(F).apply(K));
+        assertEquals(S, F(F).apply(S));
+    }
+
+    public void testApply() {
+
+    }
+
+    public void testToString() {
+    }
 }
