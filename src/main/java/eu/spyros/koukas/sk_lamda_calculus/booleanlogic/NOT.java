af3cea86d73a2f5087726ba15216ebcd588a9bd2
Spyros K <21125056+SpyrosKou@users.noreply.github.com>
2020-08-20 11:31:35 +0300

    Implemented NOT

---
+++ src/main/java/eu/spyros/koukas/sk_lamda_calculus/booleanlogic/NOT.java
@@ -0,0 +1,76 @@
+package eu.spyros.koukas.sk_lamda_calculus.booleanlogic;
+
+import eu.spyros.koukas.sk_lamda_calculus.primary.Term;
+
+import static eu.spyros.koukas.sk_lamda_calculus.booleanlogic.F.F;
+import static eu.spyros.koukas.sk_lamda_calculus.booleanlogic.T.T;
+
+/**
+ * The boolean logic NOT function.
+ *
+ *
+ * Created at 2020-08-20
+ *
+ * @author Spyros Koukas
+ */
+public final class NOT implements Term {
+    private static final String NAME = "NOT";
+    /**
+     * A singleton {@link NOT}
+     */
+    public static final NOT NOT = new NOT();
+
+    /**
+     * Hidden constructor
+     */
+    private NOT() {
+
+    }
+
+    /**
+     * @param obj the reference object with which to compare.
+     *
+     * @return {@code true} if this object is the same as the obj
+     * argument; {@code false} otherwise.
+     *
+     * @see #hashCode()
+     */
+    @Override
+    public boolean equals(final Object obj) {
+        return obj != null && (this.getClass().isInstance(obj));
+    }
+
+
+    /**
+     * @param x
+     *
+     * @return
+     */
+    public final Term apply(final Term x) {
+        return x.apply(F).apply(T);
+    }
+
+
+    public static final Term NOT() {
+        return NOT;
+    }
+
+    /**
+     * @param x
+     *
+     * @return
+     */
+    public static final Term NOT(final Term x) {
+        return NOT.apply(x);
+    }
+
+    /**
+     *
+     * @return "T"
+     */
+    @Override
+    public String toString() {
+        return NAME;
+    }
+
+}
